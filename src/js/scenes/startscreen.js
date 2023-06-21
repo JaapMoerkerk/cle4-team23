556493e3d201f0bca58ca94d103df639c88d087a
jaapi <jaapiemoerkerk@hotmail.com>
2023-06-21 14:22:11 +0200

    controls final van 21 juni

--- src/js/scenes/startscreen.js
+++ src/js/scenes/startscreen.js
@@ -32,7 +32,6 @@ export class Startscreen extends Scene{
         this.add(ground)
         const trashCan1 = new Trash()
         this.add(trashCan1)
-
         // const spawner1 = Spawner
         // this.add(spawner1)
     }
