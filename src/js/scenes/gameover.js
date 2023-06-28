4ede5c3361989d2fca75ffcdde2467ba51635ca8
jaapi <jaapiemoerkerk@hotmail.com>
2023-06-28 13:04:45 +0200

    dubbele obstacle, healthbar, collision, basically alles

--- src/js/scenes/gameover.js
+++ src/js/scenes/gameover.js
@@ -1,38 +1,48 @@
-import {Font, FontUnit, Label, Scene, TextAlign, Vector} from "excalibur";
+import {Color, Font, FontUnit, Label, Scene, TextAlign, Vector} from "excalibur";
 import {Settings} from "../settings.js";
 import {Resources} from "../resources.js";
+import {Background} from "../classes/background.js";
 
 export class Gameover extends Scene {
     onInitialize(engine) {
         const gameOverLabel = new Label({
-            text: `Game Over`,
+            text: `GAME OVER`,
             textAlign: TextAlign.Center,
-            pos: new Vector(250, 100),
+            pos: new Vector(1920/2, 300),
             font: new Font({
-                family: 'impact',
-                size: 60,
-                unit: FontUnit.Px
+                textAlign: TextAlign.Center,
+                family: 'bahnschrift',
+                size: 200,
+                unit: FontUnit.Px,
+                color: Color.Red
             })
         })
         const gameOverLabelVerhaal = new Label({
-            text:
-                        `31 maart 1943 Geallieerd vergissingsbombardement
-                   op Rotterdam Amerikaanse vliegtuigen bombarderen
-                   op 31 maart 1943 per vergissing een woonwijk van 
-                   Rotterdam-west. Het doel is het havencomplex waar 
-                   veel materiaal voor de ‘Kriegsmarine’ ligt 
-                   opgeslagen. Er vallen ongeveer 400 doden en evenveel
+            text: `
+            31 maart 1943.
+                   _______________________________
+                   
+                   Geallieerd vergissingsbombardement op Rotterdam Amerikaanse 
+                   vliegtuigen bombarderen op 31 maart 1943 per vergissing een woonwijk van 
+                   Rotterdam-west. Het doel is het havencomplex waar veel materiaal voor 
+                   de ‘Kriegsmarine’ ligt opgeslagen. Er vallen ongeveer 400 doden en evenveel
                    gewonden. In totaal gaan 2661 woningen verloren.`,
-            textAlign: TextAlign.Center,
-            pos: new Vector(200, 150),
+            pos: new Vector(1920/2 - 70, 450),
             font: new Font({
-                family: 'impact',
-                size: 40,
-                unit: FontUnit.Px
+                textAlign: TextAlign.Center,
+                family: 'bahnschrift',
+                bold: true,
+                color: Color.Black,
+                size: 50,
+                unit: FontUnit.Px,
             })
         })
+
+        const background = new Background("Background", 20)
+        this.add(background)
         this.add(gameOverLabel)
         this.add(gameOverLabelVerhaal)
+        background.graphics.opacity = 0.5
     }
 
 }
--- src/js/scenes/menuscreen.js
+++ src/js/scenes/menuscreen.js
@@ -1,4 +1,4 @@
-import {Color, Font, FontUnit, Input, Label, Scene, Vector} from "excalibur";
+import {BaseAlign, Color, Font, FontUnit, Input, Label, Scene, TextAlign, Vector} from "excalibur";
 import {Background} from "../classes/background.js";
 import {Settings} from "../settings.js";
 
@@ -11,48 +11,46 @@ export class Menuscreen extends Scene {
 
         const startMessage = new Label({
             text: "PRESS SPACE TO CONTINUE",
-            pos: new Vector(0, Settings.screenHeight - 200),
+            pos: new Vector(Settings.screenWidth/2, Settings.screenHeight - 50),
             font: new Font({
-                family: 'impact',
-                color: Color.Cyan,
-                size: 50,
-                unit: FontUnit.Px
+                textAlign: TextAlign.Center,
+                family: 'bahnschrift',
+                color: Color.DarkGray,
+                size: 60,
+                unit: FontUnit.Px,
+                bold: true
             })
         })
 
         const title = new Label({
-            text: "Parkrunner 1943",
-            pos: new Vector(220, Settings.screenHeight/2 - 100),
+            text: `PARKRUNNER 1943`,
+            pos: new Vector(Settings.screenWidth/2, Settings.screenHeight/2 - 40),
             font: new Font({
-                family: 'impact',
+                textAlign: TextAlign.Center,
+                family: 'bahnschrift',
                 color: Color.Red,
                 size: 160,
-                unit: FontUnit.Px
+                unit: FontUnit.Px,
+                // bold: true,
+                baseAlign: BaseAlign.Middle
             })
         })
-
-        startMessage.pos.x = Settings.screenWidth/2 - startMessage.getTextWidth()*2
-
-        const settings = new Label({
-            width: 190,
-            height: 50,
-            anchor: new Vector(0.002, 0.8),
-            text: 'SETTINGS',
-            pos: new Vector(
-                Settings.screenWidth - 200,
-                Settings.screenHeight - 20),
-            font: new Font({
-                family: 'impact',
-                color: Color.White,
-                size: 50,
-                unit: FontUnit.Px
-            })
-        });
-
-        // Event listener for start button click
-        settings.on("pointerup", () => {
-            //Go to settings scene
-        });
+        //
+        // const settings = new Label({
+        //     width: 190,
+        //     height: 50,
+        //     anchor: new Vector(0.002, 0.8),
+        //     text: 'SETTINGS',
+        //     pos: new Vector(
+        //         Settings.screenWidth - 200,
+        //         Settings.screenHeight - 20),
+        //     font: new Font({
+        //         family: 'impact',
+        //         color: Color.White,
+        //         size: 50,
+        //         unit: FontUnit.Px
+        //     })
+        // });
 
         // Add the label actor to the scene
         const menuBackground = new Background('Background', 50)
@@ -60,7 +58,7 @@ export class Menuscreen extends Scene {
 
         this.add(startMessage)
         this.add(title)
-        this.add(settings)
+        // this.add(settings)
     }
 
     onPostUpdate(engine, delta) {
